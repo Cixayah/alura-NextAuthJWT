04993d6fe1b467ac6ef355dc220005184ac3f96e
Cix <gabrielviniciodacosta@gmail.com>
2023-10-20 09:38:44 -0300

    First Commit

---
+++ frontend/pages/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+export default function HomeScreen() {
+  const router = useRouter();
+  // Define o estado inicial para as variáveis 'usuario' e 'senha'
+  const [values, setValues] = React.useState({
+    usuario: 'omariosouto',
+    senha: 'safepassword',
+  });
+
+  // Função que é chamada quando o valor de um campo é alterado
+  function handleChange(event) {
+    // Obtém o valor do campo e o nome do campo do evento
+    const fieldValue = event.target.value;
+    const fieldName = event.target.name;
+
+    // Atualiza o estado com os novos valores do campo
+    setValues((currentValues) => {
+      return {
+        ...currentValues,
+        [fieldName]: fieldValue,
+      };
+    });
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={(event) => {
+        // Impede o envio do formulário quando o botão é pressionado
+        event.preventDefault();
+      //  router.push('/auth-page-static');
+        router.push('/auth-page-ssr');
+      }}>
+
+        <input
+          placeholder="Usuário" name="usuario"
+          defaultValue={values.usuario} onChange={handleChange}
+        />
+        <input
+          placeholder="Senha" name="senha" type="password"
+          defaultValue={values.senha} onChange={handleChange}
+        />
+        {/*<pre>
+          {JSON.stringify(values, null, 2)}
+        </pre>*/}
+        <div>
+          <button>
+            Entrar
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
