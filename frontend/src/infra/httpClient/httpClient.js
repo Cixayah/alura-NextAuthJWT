2886ddf8176e2bd5a753bb47be88afc380153fc4
Cix <gabrielviniciodacosta@gmail.com>
2023-10-20 15:56:05 -0300

    #

--- frontend/src/infra/httpClient/httpClient.js
+++ frontend/src/infra/httpClient/httpClient.js
@@ -1,6 +1,9 @@
 export async function httpClient(fetchUrl, fetchOptions) {
     return fetch(fetchUrl,{
         ...fetchOptions,
+        headers: {
+            'Content-Type': 'application/json'
+        },
         body: fetchOptions.body ? JSON.stringify(body) : null,
     })
         .then(async(respostaDoServidor) => {
